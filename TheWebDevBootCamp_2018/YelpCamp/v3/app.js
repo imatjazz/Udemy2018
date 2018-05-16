d93df888ca568cb1a430c6c75f524cea2a96ffa8
Do <tiendo@kpmg.com.au>
2018-05-17 08:50:34 +1000

    add v3

---
+++ TheWebDevBootCamp_2018/YelpCamp/v3/app.js
@@ -0,0 +1,88 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    seedDB      = require("./seeds");
+
+//seedDB();
+
+mongoose.connect("mongodb://localhost/yelp_camp_v3");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+// Campground.create(
+//     {
+//         name: "Granite Hill", 
+//         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWcYAE8PQYP1TJM4h3Zn9R65DSVmnoGFqDogH31tLeecouCxwb2w",
+//         description: "This is a huge granite hill, no bathroom, no water. Beautiful grnaite!!!"
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else{
+//             console.log("NEWLY CREATED CAMPGROUND");
+//             console.log(campground);
+//         }
+//     }); 
+
+app.get("/", function(req,res){
+    res.render("landing");
+});
+
+//INDEX - show all campgrounds
+app.get("/campgrounds", function(req,res){
+    // Get all campground from DB
+    Campground.find({}, function(err, allCampground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds: allCampground});
+        }
+    });
+});
+
+//CREATE - add new campground to DB
+app.post("/campgrounds", function(req,res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name:name, image:image, description:desc};
+
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            // redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+//NEW - show form to create new campground
+app.get("/campgrounds/new", function(req,res){
+    res.render("new");
+});
+
+//SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req,res){
+    //find the campground with provided Id
+    Campground.findById(req.params.id).populate("comments").exec(function(err,foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundCampground);
+            res.render("show",{campground:foundCampground});
+        }
+    });
+
+    //render show template with that campground
+});
+
+app.listen("3000", function(){
+    console.log("======== YelpCamp Server Has Started =========")
+});
+
+//this is special for testing Sat 12.21am mmmm
---
+++ TheWebDevBootCamp_2018/YelpCamp/v3/seeds.js
@@ -0,0 +1,64 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment   = require("./models/comment");
+ 
+var data = [
+    {
+        name: "Cloud's Rest", 
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKT63xNw7bCkD8mZwaV8C9jnZsKKbWtFdfDjeGzr1nfOO5iC9lNw",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Desert Mesa", 
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStOsUU0KkrP2G_z7OKcp-RKcGBa8Fb7raXzd7S7WI1MeW8gz_WoQ",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHwz-WxzQfD-ymcndniolj4oVEJN4HeLUUhDGpUePnV82q6P6-",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+]
+ 
+function seedDB(){
+   //Remove all campgrounds
+   Campground.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        Comment.remove({}, function(err) {
+            if(err){
+                console.log(err);
+            }
+            console.log("removed comments!");
+             //add a few campgrounds
+            data.forEach(function(seed){
+                Campground.create(seed, function(err, campground){
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log("added a campground");
+                        //create a comment
+                        Comment.create(
+                            {
+                                text: "This place is great, but I wish there was internet",
+                                author: "Homer"
+                            }, function(err, comment){
+                                if(err){
+                                    console.log(err);
+                                } else {
+                                    campground.comments.push(comment);
+                                    campground.save();
+                                    console.log("Created new comment");
+                                }
+                            });
+                    }
+                });
+            });
+        });
+    }); 
+    //add a few comments
+}
+ 
+module.exports = seedDB;
